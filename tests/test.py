26be3b59bd2c743a36722c59d66e2204c9d850ae
kkent030315 <hrn832@protonmail.com>
2021-03-31 07:35:16 +0900

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,28 @@
 import sys
 sys.path.append("../src/")
 
-from twitcasting_realtime_chat import TwcsRealtimeChat 
+import threading
+import time
+import os
+import signal
+from twitcasting_realtime_chat import TwcsRealtimeChat
 
 
-def main() -> None:
+def thread_worker():
     try:
         instance = TwcsRealtimeChat(auto_reconnect=True)
         instance.run(user_id="izayoikaede")
     except KeyboardInterrupt:
         pass
+
+
+def main():
+    thread = threading.Thread(target=thread_worker)
+    thread.start()
+
+    time.sleep(5)
+
+    os.kill(os.getpid(), signal.SIGKILL)
 
 
 if __name__ == "__main__":
